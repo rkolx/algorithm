cc27ae851c07c3785457a176d44d8c816bac54c7
rkolx <olx.rko.o@gmail.com>
2022-01-05 22:08:33 +0900

    feat: 1076 문재 해결

---
+++ src/week1/B1076.java
@@ -0,0 +1,33 @@
+package week1;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class B1076 {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String[]> setData= new HashMap<>();
+        setData.put("black", new String[]{"0","1"});
+        setData.put("brown", new String[]{"1","10"});
+        setData.put("red", new String[]{"2","100"});
+        setData.put("orange", new String[]{"3","1000"});
+        setData.put("yellow", new String[]{"4","10000"});
+        setData.put("green", new String[]{"5","100000"});
+        setData.put("blue", new String[]{"6","1000000"});
+        setData.put("violet", new String[]{"7","10000000"});
+        setData.put("grey", new String[]{"8","100000000"});
+        setData.put("white", new String[]{"9","1000000000"});
+
+        Scanner sc = new Scanner(System.in);
+
+        String color1 = setData.get(sc.next())[0];
+        String color2 = setData.get(sc.next())[0];
+        String color3 = setData.get(sc.next())[1];
+
+        long result = Long.parseLong(color1+color2)*Long.parseLong(color3);
+
+        System.out.println(result);
+
+    }
+}
