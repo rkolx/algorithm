1013af808502c9c3a659867d2004bdd7e15c24f0
rkolx <olx.rko.o@gmail.com>
2022-01-12 19:31:46 +0900

    feat: ReverseInteger 문제 해결

---
+++ src/W2/ReverseInteger.java
@@ -0,0 +1,23 @@
+package W2;
+
+public class ReverseInteger {
+    public int reverse(int x){
+
+        StringBuilder sb = new StringBuilder();
+        String str = String.valueOf(x);
+        Long result;
+
+        if (str.charAt(0)=='-'){
+            str=str.substring(1);
+            sb.append(str).reverse().insert(0,'-');
+        }else {
+            sb.append(str).reverse();
+        }
+        result = Long.parseLong(sb.toString());
+        if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE){
+            return 0;
+        }
+
+        return Integer.parseInt(String.valueOf(result));
+    }
+}
