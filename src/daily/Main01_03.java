3e86036a34375df589df30410d5ae10378ec2e3c
rkolx <olx.rko.o@gmail.com>
2022-02-02 21:42:07 +0900

    feat: 문장 속 단어

---
+++ src/daily/Main01_03.java
@@ -0,0 +1,23 @@
+package daily;
+import java.util.Scanner;
+
+public class Main01_03 {
+
+
+    public String Solution(String s){
+
+        String answer="";
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Scanner in=new Scanner(System.in);
+        int input = in.nextInt();
+        System.out.println(input);
+
+    }
+}
+
+
