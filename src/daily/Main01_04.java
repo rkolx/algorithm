c61d9d90f753d2d6dd1b2fcaec98e5363423c8e0
rkolx <olx.rko.o@gmail.com>
2022-02-04 16:47:37 +0900

    feat: 단어뒤집 (여러 해결방법)

--- src/daily/Main01_04.java
+++ src/daily/Main01_04.java
@@ -5,11 +5,34 @@ import java.util.Scanner;
 
 public class Main01_04 {
 
+    //StringBuilder
     public ArrayList<String> Solution(int num, String[] str){
         ArrayList<String> answer = new ArrayList<>();
 
         for (String x: str) {
             String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+    //하나하나 뒤집기
+    public ArrayList<String> Solution2(int num, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+
+        for (String x: str) {
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = x.length()-1;
+
+            while (lt < rt){
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
             answer.add(tmp);
         }
 
