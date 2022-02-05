39c36100a54e25a6b1c7649fa7717bbf5f5fea02
rkolx <olx.rko.o@gmail.com>
2022-02-06 00:34:05 +0900

    feat: 특정문자뒤집기 -> 특수문자일때 처리 다시 공부해볼 것

--- src/daily/Main01_05.java
+++ src/daily/Main01_05.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class Main01_05 {
     public char[] Solution(String s){
 
-        char[] answer = new char[s.length()];
+        //char[] answer = new char[s.length()];
+        char[] answer = s.toCharArray();
         int first = 0;
         int last = s.length()-1;
 
-        for (int i = 0; i <answer.length; i++) {
-            answer[i] = s.charAt(i);
+        while (first < last){
+             if (!Character.isAlphabetic(answer[first])){
+                 first++;
+             } else if (!Character.isAlphabetic(answer[last])){
+                 last--;
+             } else {
+                 char tmp = answer[first];
+                 answer[first] = answer[last];
+                 answer[last] = tmp;
+                 first++;
+                 last--;
+             }
         }
 
-        while (first < last){
-            char tmp = answer[first];
-            answer[first] = answer[last];
-            answer[last] = tmp;
-            first++;
-            last--;
-        }
         return answer;
     }
 
