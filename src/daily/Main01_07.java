fbf0ce86c0931205633e7b423a82f2c4b3dde072
rkolx <olx.rko.o@gmail.com>
2022-02-09 09:51:41 +0900

    feat: 회문문자열 다시 풀어보기

--- src/daily/Main01_07.java
+++ src/daily/Main01_07.java
@@ -1,4 +1,25 @@
 package daily;
 
+import java.util.Scanner;
+
 public class Main01_07 {
+
+    public String Solution(String s){
+        String answer = "YES";
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(s.length()-i-1)) {
+                return "NO";
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args){
+        Main01_07 m = new Main01_07();
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        System.out.println(m.Solution(input));
+    }
 }
