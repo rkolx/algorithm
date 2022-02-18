56f98538fe6ee9be9fd3e4056b5dacd7bffbccd0
rkolx <olx.rko.o@gmail.com>
2022-02-18 22:10:57 +0900

    feat: 가장 짧은 문자 거리

--- src/daily/Main01_10.java
+++ src/daily/Main01_10.java
@@ -1,4 +1,12 @@
 package daily;
 
+import java.util.Scanner;
+
 public class Main01_10 {
+    public static void main(String[] args){
+
+        Scanner in=new Scanner(System.in);
+        String input = in.next();
+        System.out.println(input);
+    }
 }
