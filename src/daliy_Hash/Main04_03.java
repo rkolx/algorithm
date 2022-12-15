4f58edc2bc822fa4f48ce300806e2d8f8942040c
rkolx <olx.rko.o@gmail.com>
2022-12-16 01:13:59 +0900

    feat: 매출액의 종류

---
+++ src/daliy_Hash/Main04_03.java
@@ -0,0 +1,49 @@
+package daliy_Hash;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main04_03 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < K - 1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        int lt = 0;
+        List<Integer> count = new ArrayList<>();
+        for (int rt = K - 1; rt < N; rt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            count.add(map.size());
+            map.put(arr[lt], map.getOrDefault(arr[lt], 0) - 1);
+
+            if (map.get(arr[lt]) == 0) {
+                map.remove(arr[lt]);
+            }
+            lt++;
+        }
+        for (Integer i : count) {
+            System.out.print(i + " ");
+        }
+    }
+
+
+}
