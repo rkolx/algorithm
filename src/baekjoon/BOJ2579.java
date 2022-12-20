33aa83f60f4daa327f259572860e62dbfd74116a
rkolx <olx.rko.o@gmail.com>
2022-12-20 23:02:58 +0900

    feat: 계단오르기(2579)

--- src/baekjoon/BOJ2579.java
+++ src/baekjoon/BOJ2579.java
@@ -5,26 +5,23 @@ import java.io.*;
 public class BOJ2579 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
+
         int[] stairs = new int[n + 1];
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
         }
 
         int[] dp = new int[n+1];
         dp[1] = stairs[1];
 
-        for (int i = 2; i <= n; i++) {
-            if (i == 2) {
-                dp[2] = stairs[1] + stairs[2];
-            } else if (i == 3) {
-                dp[3] = Math.max(stairs[1], stairs[2] + stairs[3]);
-            } else {
-                dp[i] = Math.max(dp[i - 3] + stairs[i -1], dp[i -2]) + stairs[i];
-            }
+        if (n >= 2) {
+            dp[2] = stairs[1] + stairs[2];
         }
-        bw.write(dp[n]);
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3] + stairs[i]-1) + stairs[i];
+        }
+        System.out.println(dp[n]);
     }
 }
