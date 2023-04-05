8fae319a6c9a5e4095d6db79a79580c69a285401
rkolx <olx.rko.o@gmail.com>
2023-04-05 21:24:37 +0900

    feat: 좌표

---
+++ src/daliy_SortingAndSearching/Main06_07.java
@@ -0,0 +1,50 @@
+package daliy_SortingAndSearching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+class Point implements Comparable<Point> {
+
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            return y - o.y;
+        }
+        return x - o.x;
+    }
+}
+
+public class Main06_07 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points.add(new Point(x, y));
+        }
+        Collections.sort(points);
+        for (Point o : points) {
+            System.out.println(o.x + " " + o.y);
+        }
+    }
+
+}
