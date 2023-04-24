9fee39b485c6ffb3091b00b43f85caaac0605efb
rkolx <olx.rko.o@gmail.com>
2023-04-24 22:09:05 +0900

    feat: 이진수 출력(재귀)

---
+++ src/inflearn/section07/Main03.java
@@ -0,0 +1,26 @@
+package inflearn.section07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main03 {
+
+    public static void main(String[] args) throws IOException {
+        Main03 m = new Main03();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(m.DFS(n));
+    }
+
+    public int DFS(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+           return n * DFS(n -1);
+        }
+    }
+
+}
