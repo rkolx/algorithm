cd935598a489944f9faf6606fc4a05ec4dcb5a32
rkolx <olx.rko.o@gmail.com>
2023-04-25 16:16:48 +0900

    feat: 피보나치수열(메모제이션)

--- src/inflearn/section07/Main04.java
+++ src/inflearn/section07/Main04.java
@@ -6,22 +6,29 @@ import java.io.InputStreamReader;
 
 public class Main04 {
 
+    static int[] tmp;
+    public int DFS(int n) {
+        if (tmp[n] > 0) {
+            return tmp[n];
+        }
+        if (n == 1) {
+            return tmp[n] = 1;
+        } else if (n == 2) {
+            return tmp[n] = 1;
+        } else {
+            return tmp[n] = DFS(n - 1) + DFS(n - 2);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Main04 m = new Main04();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
+        tmp = new int[n + 1];
+        m.DFS(n);
         for (int i = 1; i <= n; i++) {
-            System.out.print(m.DFS(i) + " ");
-        }
-    }
-
-    public int DFS(int n) {
-        if (n <= 1) {
-            return n;
-        } else {
-            return DFS(n - 1) + DFS(n - 2);
+            System.out.print(tmp[i] + " ");
         }
     }
 }
